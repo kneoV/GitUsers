b0d50836bf867f571886fba9e223c200682647df
Vitaliy Voronok <mobd@isddesign.com>
2016-06-15 21:42:13 +0300

    Initial commit

---
+++ GitUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUsers
+//
+//  Created by Vitaliy Voronok on 6/15/16.
+//  Copyright © 2016 Vitaliy Voronok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
