7da076cbd678753f72a5d854262eb7eb4f973825
Vitaliy Voronok <kneo82@mail.ru>
2017-07-21 08:36:01 +0300

    Initial Commit

---
+++ Mines/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mines
+//
+//  Created by Vitaliy Voronok on 7/21/17.
+//  Copyright © 2017 Vitaliy Voronok. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
